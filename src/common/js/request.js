e5c2670f0b3bbc3bc0f758bb790b7ccc101474da
soupJian <soupjian@163.com>
2022-04-28 15:11:24 +0800

    feat:接口迁移vercel

--- src/common/js/request.js
+++ src/common/js/request.js
@@ -1,12 +1,29 @@
 import axios from 'axios'
+const baseUrl = 'https://music-soupjian.vercel.app'
+const service = axios.create({
+  withCredentials: true,
+  method: 'get'
+})
+service.interceptors.request.use(config=>{
+  config.params = {
+    ...config.params,
+    realIP: '116.25.146.177',
+  }
+  return config
+})
+
 export const request = (params) => {
-    const baseUrl = '/api';
-    let url = `${baseUrl}${params.url}`;
-    return axios.get(url);
-  };
-  export const requestCookie = (params) => {
-    const baseUrl = '/api';
-    const cookie = localStorage.getItem('cookie');
-    let url = `${baseUrl}${params.url}?cookie=${cookie}`;
-    return axios.get(url);
-  };
+  // const baseUrl = '/api';
+  let url = `${baseUrl}${params.url}`;
+  return service({
+    url
+  });
+};
+export const requestCookie = (params) => {
+  // const baseUrl = '/api';
+  const cookie = localStorage.getItem('cookie');
+  let url = `${baseUrl}${params.url}?cookie=${cookie}`;
+  return service({
+    url
+  });
+};
